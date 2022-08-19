7e374ed6910a365f50422d981545f3b4662959d9
raushanhvt <raushan.kumar@hexaviewtech.com>
2022-08-19 11:47:18 +0530

    read from excel

--- src/main/java/com/example/poi/ReadExcelDemo.java
+++ src/main/java/com/example/poi/ReadExcelDemo.java
@@ -1,9 +1,45 @@
 package com.example.poi;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ReadExcelDemo {
 
     public static void main(String[] args) {
         
+        try {
+            FileInputStream file = new FileInputStream(new File("emp.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            while(rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+
+                    }
+
+                }
+                System.out.println("");
+            }
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
